56cc15347b37374f812fc87a20c84665ddccefd0
Boris Searles <boris@lucidgardens.com>
2012-03-01 23:16:51 +0100

    add basic package setup script for ubuntu

---
+++ ubuntu_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# setup of basic packages for clean ubuntu systems
+
+apt-get install git curl wget vim
+
+# prerequisites for tmux
+apt-get install libevent-1.4 libevent-dev libncurses5-dev
+
+cd /tmp
+wget http://downloads.sourceforge.net/tmux/tmux-1.6.tar.gz
+cd tmux-1.6
+./configure
+make install
+cd ..
+rm -rf tmux-1.6
+rm tmux-1.6.tar.gz
