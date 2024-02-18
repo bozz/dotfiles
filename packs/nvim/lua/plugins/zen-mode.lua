518d8aa7064485c3462d168ab558eaf880e09eaa
Boris Searles <boris@searles.de>
2024-02-18 01:26:34 +0100

    feat: nvim plugin updates

--- packs/nvim/lua/plugins/zen-mode.lua
+++ packs/nvim/lua/plugins/zen-mode.lua
@@ -14,6 +14,14 @@ return {
         tmux = true,
         kitty = { enabled = true, font = "+2" },
       },
+      -- callback where you can add custom code when the Zen window opens
+      on_open = function(win)
+        vim.cmd("Pencil")
+      end,
+      -- callback where you can add custom code when the Zen window closes
+      on_close = function()
+        vim.cmd("PencilToggle")
+      end,
     },
     keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
   },
