8216708f0a639ebb1089528f00a7287316027379
Boris Searles <boris@searles.de>
2024-11-28 21:11:23 +0100

    feat: update obsidian plugin config

--- packs/nvim/lua/plugins/obsidian.lua
+++ packs/nvim/lua/plugins/obsidian.lua
@@ -6,7 +6,18 @@ return {
 
     lazy = true,
 
-    ft = "markdown",
+    -- ft = "markdown",
+    event = {
+      "BufReadPre " .. vim.fn.expand("~") .. "Documents/SynologyDrive/obsidian/oro/*.md",
+      "BufNewFile " .. vim.fn.expand("~") .. "Documents/SynologyDrive/obsidian/oro/*.md",
+    },
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
 
     dependencies = {
       -- Required.
@@ -20,6 +31,22 @@ return {
           path = "~/Documents/SynologyDrive/obsidian/oro/",
         },
       },
+
+      -- Where to put new notes created from completion. Valid options are
+      --  * "current_dir" - put new notes in same directory as the current buffer.
+      --  * "notes_subdir" - put new notes in the default notes subdirectory.
+      new_notes_location = "current_dir",
+
+      -- Optional, customize how wiki links are formatted. You can set this to one of:
+      --  * "use_alias_only", e.g. '[[Foo Bar]]'
+      --  * "prepend_note_id", e.g. '[[foo-bar|Foo Bar]]'
+      --  * "prepend_note_path", e.g. '[[foo-bar.md|Foo Bar]]'
+      --  * "use_path_only", e.g. '[[foo-bar.md]]'
+      -- Or you can set it to a function that takes a table of options and returns a string, like this:
+      wiki_link_func = function(opts)
+        return require("obsidian.util").wiki_link_id_prefix(opts)
+      end,
+
       completion = {
         -- Set to false to disable completion.
         nvim_cmp = true,
@@ -27,25 +54,20 @@ return {
         -- Trigger completion at 2 chars.
         min_chars = 2,
 
-        -- Where to put new notes created from completion. Valid options are
-        --  * "current_dir" - put new notes in same directory as the current buffer.
-        --  * "notes_subdir" - put new notes in the default notes subdirectory.
-        new_notes_location = "current_dir",
-
         -- Control how wiki links are completed with these (mutually exclusive) options:
         --
         -- 1. Whether to add the note ID during completion.
         -- E.g. "[[Foo" completes to "[[foo|Foo]]" assuming "foo" is the ID of the note.
         -- Mutually exclusive with 'prepend_note_path' and 'use_path_only'.
-        prepend_note_id = true,
+        -- prepend_note_id = true,
         -- 2. Whether to add the note path during completion.
         -- E.g. "[[Foo" completes to "[[notes/foo|Foo]]" assuming "notes/foo.md" is the path of the note.
         -- Mutually exclusive with 'prepend_note_id' and 'use_path_only'.
-        prepend_note_path = false,
+        -- prepend_note_path = false,
         -- 3. Whether to only use paths during completion.
         -- E.g. "[[Foo" completes to "[[notes/foo]]" assuming "notes/foo.md" is the path of the note.
         -- Mutually exclusive with 'prepend_note_id' and 'prepend_note_path'.
-        use_path_only = false,
+        -- use_path_only = false,
       },
       -- templates = {
       --   subdir = "_assets/templates",
