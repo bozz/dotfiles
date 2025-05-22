028dfa9ed9b8d5cd41b912e30e592fbaf3b5f61d
Boris Searles <boris@searles.de>
2025-05-22 20:34:50 +0200

    nvim: add conform and tailwind plugins

---
+++ packs/nvim/lua/plugins/conform.lua
@@ -0,0 +1,14 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      astro = { "prettier" },
+    },
+  },
+}
+
+-- require("conform").setup({
+--   formatters_by_ft = {
+--     astro = { "prettier" },
+--   },
+-- })
